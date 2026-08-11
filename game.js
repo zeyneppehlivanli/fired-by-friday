33dadf2af250b784c794ef2807e147df3d2c13e7
Zeynep Pehlivanlı <pehlivanlizeynep@gmail.com>
2026-08-11 20:17:20 +0300

    Mobil ekran uyumluluğu eklendi

--- game.js
+++ game.js
@@ -1349,8 +1349,12 @@ class MainScene extends Phaser.Scene {
 
 const config = {
     type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1280,
+        height: 720
+    },
     parent: 'game-container',
     backgroundColor: '#c8d6e5',
     scene: MainScene
